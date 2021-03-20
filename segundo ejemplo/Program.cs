f182d196d48bc69793c95337ec4b88d5bd603529
GL0R1A <79273578+GL0R1A@users.noreply.github.com>
2021-03-19 20:46:02 -0600

    tareaejercicio5
    tareas

---
+++ segundo ejemplo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace segundo_ejemplo
+{
+    class Program
+    {
+        public static void dibuja(int Cantidad)
+        {
+            int FILAB = 35;
+            int COLUMF = 25;
+            int INTERNO = 5;
+            int COLUMI = 5;
+            int F, C, A;
+
+            for (A = 1; A <= Cantidad; A++)
+            {
+                for (F = COLUMI; F <= COLUMF; F++)
+                {
+                    Console.SetCursorPosition(F, FILAB); Console.Write("$");
+                    Console.SetCursorPosition(F, INTERNO); Console.Write("#");
+                }
+                for (C = COLUMI; C <= FILAB; C++)
+                {
+                    Console.SetCursorPosition(COLUMI, C); Console.Write("***");
+                    Console.SetCursorPosition(COLUMF, C); Console.Write("XXX");
+                }
+                INTERNO = INTERNO + 1; 
+                FILAB = FILAB - 1;
+                COLUMI = COLUMI + 1;
+                COLUMF = COLUMF - 1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int tam = 1; tam < 5; tam++)
+            {
+                dibuja(tam);
+                Thread.Sleep(50); 
+            }
+
+        }
+    } 
+}
+
+
+ 
+
+
+
+
+
+
---
+++ tarea ejemplo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace tarea_ejemplo
+{
+    class Program
+    {
+        public static string Mid(string parametro, int startindex, int Length)
+        {
+            string result = parametro.Substring(startindex, Length);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string NOMBRE;
+            string CARACTER;
+            int MOVI= 0;
+            int INICIAR = 0;
+            int CADENA = 0;
+            int LIM = 0;
+            Console.Write("Ingrese su nombre: ");
+            NOMBRE = Console.ReadLine();
+            Console.SetCursorPosition(1, 12); 
+            Console.Write(NOMBRE);
+            CADENA = NOMBRE.Length;
+            LIM = 60;
+            NOMBRE = NOMBRE.ToUpper();
+            for (INICIAR= NOMBRE.Length; INICIAR >= 1; INICIAR--)
+            {
+                CARACTER = Mid(NOMBRE, INICIAR - 1, 1);
+                for (MOVI = CADENA; MOVI <= LIM; MOVI++)
+                {
+                    Console.SetCursorPosition(MOVI, 12);
+                    Console.Write(" " + CARACTER);
+                        Thread.Sleep(50);
+                }
+                LIM = LIM - 1;
+                CADENA = CADENA - 1;
+            }
+        }
+    }
+}
+        
+
+
+
+
+
+
+
+
+
+
+
